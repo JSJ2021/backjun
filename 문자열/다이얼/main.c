f8e10b9680289f06664ec5d5d1e1b6309ca93591
Soung Jea Joung <sjoung@c2r8s7.42gyeongsan.kr>
2025-04-23 16:40:24 +0900

    dd

--- 문자열/다이얼/main.c
+++ 문자열/다이얼/main.c
@@ -2,5 +2,47 @@
 
 int main(void)
 {
-    
+    char c[15];
+    int i;
+    int cnt = 0;
+
+    i = 0;
+    scanf("%s", c);
+    while(i < 15)
+    {
+        if (*(c + i) >= 'A' && *(c + i) <= 'C')
+        {
+            cnt += 3;
+        }
+        else if (*(c + i) >= 'D' && *(c + i) <= 'F')
+        {
+            cnt += 4;
+        }
+        else if (*(c + i) >= 'G' && *(c + i) <= 'I')
+        {
+            cnt += 5;
+        }
+        else if (*(c + i) >= 'J' && *(c + i) <= 'L')
+        {
+            cnt += 6;
+        }
+        else if (*(c + i) >= 'M' && *(c + i) <= 'O')
+        {
+            cnt += 7;
+        }
+        else if (*(c + i) >= 'P' && *(c + i) <= 'S')
+        {
+            cnt += 8;
+        }
+        else if (*(c + i) >= 'T' && *(c + i) <= 'V')
+        {
+            cnt += 9;
+        }
+        else if (*(c + i) >= 'W' && *(c + i) <= 'Z')
+        {
+            cnt += 10;
+        }
+        i++;
+    }
+    printf("%d",cnt);
 }
